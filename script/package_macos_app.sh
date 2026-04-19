2f9a85b4298be6ff3b79f0bbe9de36d99ab48f42
lary <larysong@boyaa.com>
2026-04-19 19:57:46 +0800

    chore: bump app version for refresh release

--- script/package_macos_app.sh
+++ script/package_macos_app.sh
@@ -42,9 +42,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<EOF
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.1.1</string>
+  <string>0.1.2</string>
   <key>CFBundleVersion</key>
-  <string>2</string>
+  <string>3</string>
   <key>LSMinimumSystemVersion</key>
   <string>13.0</string>
   <key>LSUIElement</key>
